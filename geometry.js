b4cff6239eefb827b3920e8d498938a0ae716986
lightfulhuang <lightfulhuang@gmail.com>
2021-02-07 16:37:26 +0800

    movable

--- geometry.js
+++ geometry.js
@@ -31,34 +31,33 @@ class Point extends GeomObj{
 		super()
 		this.x = x
 		this.y = y
-		this.show = function(){
-			geometry.field.selectAll("circle")
-			        .data([this]).enter()
-				.append("circle")
-				  .attr("r", 5)
-				  .attr("cx", d=>d.x)
-				  .attr("cy", d=>d.y)
-				.attr("fill", "yellow")
-				.attr("stroke", "black")
-				.call(
-					d3.drag()
-					  .on("drag", function(e){
-						e.subject.update(
-							this,
-							e.subject.x+e.dx,
-							e.subject.y+e.dy
-						)
-					  })
-				)
-		}
 		this.show()
-		this.update = function(element, x, y){
-			this.x = x
-			this.y = y
-			d3.select(element)
-			  .attr("cx", x)
-			  .attr("cy", y)
-		}
+	}
+	update(element, x, y){
+		this.x = x
+		this.y = y
+		d3.select(element)
+		  .attr("cx", x)
+		  .attr("cy", y)
+	}
+	show(){
+		let thePoint = this
+		geometry.field.append("circle")
+			  .attr("r", 7)
+			  .attr("cx", this.x)
+			  .attr("cy", this.y)
+			.attr("fill", "yellow")
+			.attr("stroke", "black")
+			.call(
+				d3.drag()
+				  .on("drag", function(e){
+					thePoint.update(
+						this,
+						thePoint.x+e.dx,
+						thePoint.y+e.dy
+					)
+				  })
+			)
 	}
 }
 class LineSegment extends GeomObj{
