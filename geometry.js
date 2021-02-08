d29cefdba2ef61254c4f1c3e2470321d737577a5
chrysalistw <lightfulhuang@gmail.com>
2021-02-09 01:47:31 +0800

    refactor(?) done

--- geometry.js
+++ geometry.js
@@ -2,8 +2,7 @@ import "./d3.js"
 import { GeomObj } from "./geometry-object.js"
 import { Point } from "./geometry-point.js"
 import { LineSegment } from "./geometry-linesegment.js"
-//.import { Bisector } from "./geometry-bisector.js"
-var Bisector = null
+import { Bisector } from "./geometry-bisector.js"
 export { Point, LineSegment, Bisector }
 var geometry = {}
 export default geometry
