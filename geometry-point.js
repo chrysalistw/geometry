d0ab7962e091c3bda5f111fd89ae4333c66f9b4f
chrysalistw <lightfulhuang@gmail.com>
2021-02-15 10:09:30 +0800

    attach to circle

--- geometry-point.js
+++ geometry-point.js
@@ -6,10 +6,13 @@ class Point extends GeomObj{
 		super()
 		this.x = x
 		this.y = y
+		this.fakePosition = {x: x, y: y}
 		this.child = []
 		this.show()
 	}
 	update(){
+		this.x = this.fakePosition.x
+		this.y = this.fakePosition.y
 		d3.select(this.element)
 		  .attr("cx", this.x)
 		  .attr("cy", this.y)
@@ -31,31 +34,36 @@ class Point extends GeomObj{
 			)
 			.call(
 				d3.drag()
+				  .on("start", function(e){
+					thePoint.dragging = true
+					thePoint.fakePosition = {
+						x: thePoint.x,
+						y: thePoint.y
+					}
+					console.log(thePoint.fakePosition)
+				  })
 				  .on("drag", function(e){
-					thePoint.x += e.dx
-					thePoint.y += e.dy
+					thePoint.fakePosition.x += e.dx
+					thePoint.fakePosition.y += e.dy
 					thePoint.update()
+				  })
+				  .on("end", function(e){
+					thePoint.dragging = false
 				  })
 			)
 	}
 }
-Point.prototype.attachToLS = function(ls){ // attach to a line segment
+Point.prototype.attachToLS = function(ls){
 	this.attached = true
-	//project this to line segment
-	let a = ls.v0, b = ls.v1
-	let m = b.x-a.x!==0?(b.y-a.y)/(b.x-a.x):"infinity"
-	let c = a.y-m*a.x
 	ls.child.push(this)
-	let thisx = (this.x+m*this.y-m*c)/(1+m*m)
-	let thisy = (m*this.x+m*m*this.y+c)/(1+m*m)
-	this.x = thisx, this.y = thisy
-	this.update()
 	this.update = function(){
 		let a = ls.v0, b = ls.v1
 		let m = b.x-a.x!==0?(b.y-a.y)/(b.x-a.x):"infinity"
 		let c = a.y-m*a.x
-		let thisx = (this.x+m*this.y-m*c)/(1+m*m)
-		let thisy = (m*this.x+m*m*this.y+c)/(1+m*m)
+		//let thisx = (this.x+m*this.y-m*c)/(1+m*m)
+		//let thisy = (m*this.x+m*m*this.y+c)/(1+m*m)
+		let thisx = (this.fakePosition.x+m*this.fakePosition.y-m*c)/(1+m*m)
+		let thisy = (m*this.fakePosition.x+m*m*this.fakePosition.y+c)/(1+m*m)
 		this.x = thisx, this.y = thisy
 		d3.select(this.element)
 		  .attr("cx", this.x)
@@ -64,12 +72,14 @@ Point.prototype.attachToLS = function(ls){ // attach to a line segment
 			c.update()
 		})
 	}
+	this.update()
 }
 Point.prototype.attachToCircle = function(c){
+	this.attached = true
 	c.child.push(this)
 	this.update = function(){
-		let _x = this.x-c.center.x
-		let _y = this.y-c.center.y
+		let _x = this.fakePosition.x-c.center.x
+		let _y = this.fakePosition.y-c.center.y
 		let _length = Math.sqrt(_x*_x+_y*_y)
 		let r = c.radius()
 		this.x = c.center.x+r/_length*_x
